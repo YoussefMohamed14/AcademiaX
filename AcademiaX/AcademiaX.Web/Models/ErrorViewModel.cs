d9531a62457f57f7f1dfb6b4cc22154573f772c5
Youssef Mohamed <ym051618@gmail.com>
2026-07-24 22:35:20 +0300

    Initial solution structure

---
+++ AcademiaX/AcademiaX.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace AcademiaX.Web.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
